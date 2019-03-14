ed210b26ba3f8f3049c3f51f7361c984c82af4f0
kamil <tyrekkamil@gmail.com>
2019-03-14 20:49:04 +0100

    v0.22 - dla x3

--- src/Polynomial.py
+++ src/Polynomial.py
@@ -22,35 +22,28 @@ def mieszaj(p, q):
 
 
 
+
 def sprawdz(pq, polynom):
     trueOrFalse = []
-
-    wynik = 0
-    polynom[::-1]
     for x in range(len(pq)):
+        wynik = 0;
         print(polynom)
         for y in range(len(polynom)):
-            wynik = wynik + pq[x]*(polynom[y]) ** y
+            wynik = wynik + polynom[y]*(pq[x] ** (y))
             print(wynik)
         if wynik == 0:
-            trueOrFalse.append(True)
-        else:
-            trueOrFalse.append(False)
+            trueOrFalse.append(pq[x])
+
     return trueOrFalse
 
 
-x0 = 1
-x1 = -2
-x2 = -1
-x3 =  2
+x0 = 6
+x1 = -5
+x2 = -2
+x3 =  1
 polynom = [x0, x1, x2, x3]
 p = pier(x3)
 q = pier(x0)
 
-print(q)
-print(p)
-
 pq = mieszaj(p, q)
 print(sprawdz(pq, polynom))
-
-print(pq)
