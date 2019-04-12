a60e40cd43a00fd75711fabcbfd0c6040d6b9c5e
kamil <tyrekkamil@gmail.com>
2019-04-12 18:30:02 +0200

    Fractions

--- src/Polynomial.py
+++ src/Polynomial.py
@@ -1,4 +1,6 @@
-from Crypto.Util.number import bignum
+#from Crypto.Util.number import bignum
+import math
+from fractions import Fraction, gcd
 
 
 def pier(p):
@@ -14,20 +16,22 @@ def pier(p):
 def mieszaj(p, q):
     mieszanka = []
     minusy = []
+
     for x in range(len(p)):
         for y in range(len(q)):
-            a = q[y] / p[x]
+            a = Fraction(q[y],p[x])
             if a not in mieszanka:
                 mieszanka.append(a)
     for x in range(len(mieszanka)):
         minusy.append(mieszanka[x] * -1)
     mieszanka.extend(minusy)
+    print("Wszystkie mozliwe:",mieszanka)
     return mieszanka
 
 def sprawdz(pq, polynom):
     trueOrFalse = []
     for x in range(len(pq)):
-        wynik = 0;
+        wynik = Fraction(0,1);
         for y in range(len(polynom)):
             wynik = wynik + polynom[y] * (pq[x] ** (y))
         if wynik == 0:
@@ -54,21 +58,27 @@ def main():
         if item == '':
             break
         else:
-            item = bignum(item)
+            item = int(item)
             polynom.append(item)
+
     # print(polynom)
     p = pier(polynom[len(polynom) - 1])
     q = pier(polynom[0])
     pq = mieszaj(p, q)
-
     wynik = sprawdz(pq, polynom)
-    print("Pierwiastki rownania to:\n", wynik)
+
+    print("Pierwiastkami równania są:")
+    for i in wynik:
+        print(i)
 
     while(len(wynik) > 0):
         polynom = der(polynom)
         wynik2 = sprawdz(wynik, polynom)
+
         if len(wynik2) >= 1:
-            print(wynik2)
+            for i in wynik2:
+                print(i)
+
         wynik = wynik2;
 
 
